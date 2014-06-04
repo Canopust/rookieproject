0d3768b543e0f917b6ffc1a5006f19e1c9e9a39d
Tim Du <canopus.t@gmail.com>
2014-06-04 22:13:54 +0800

    This is third time

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
 a=[]
-
-for n in range(2, 100000000):
+for n in range(2, 10000):
     for x in range(2, n):
         if n % x == 0:
             break
     else:
         # loop fell through without finding a factor
         a.append(n)
+
 print a
 
 
-print hello world
+b=[1,2,3,4,5,6,7,8,9,10]
+b.append(11)
+b.insert(3,"x")
+print b
